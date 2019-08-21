314778cf2de6ccad1ada44d64349a936a5a3f95f
viniferraria <vferraria10@gmail.com>
2019-08-21 17:59:24 -0300

    part_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Hello Rectangle')
-//           ),
-          body: HelloRectangle(),
-      ),
-    ),
-  );
+import 'package:tutorial_udemy/category.dart';
+
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+void main() {
+  runApp(UnitConverterApp());
 }
 
-class HelloRectangle extends StatelessWidget {
+class UnitConverterApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 1080,
-        width: 1000,
-        child: Center(
-          child: Text(
-            'Hello',
-            style: TextStyle(fontSize: 80.0),
-            textAlign: TextAlign.center,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          child: Category(
+            name: _categoryName,
+            color: _categoryColor,
+            iconlocation: _categoryIcon,
           ),
         ),
       ),
